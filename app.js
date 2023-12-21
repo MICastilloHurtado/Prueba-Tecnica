733d7e1b3c1200fce9446bc1cc10ba37dd9fed11
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-12-20 22:49:31 -0500

    pensando

--- app.js
+++ app.js
@@ -79,6 +79,19 @@ function filtrarSenalesPorExpresionRegular(signals, expresionRegular) {
     return signals.filter((senal) => expresion.test(senal.MMSPATH));
 }
 
+
+
+// 4.Definir una función que retorne un número que mida la capacidad de una expresión de
+//entrada para filtrar una señal y excluir el resto.
+
+function medirCapacidadExpresion(expresionRegular, signals) {
+    const expresion = new RegExp(expresionRegular, 'i');
+    const senalesFiltradas = signals.filter((senal) => expresion.test(senal.MMSPATH));
+    
+    // Retornar el número de señales coincidentes
+    return senalesFiltradas.length;
+}
+
 const rutaArchivoCSV = './archivo de entrada.csv';
 
 cargarDatosDesdeCSV(rutaArchivoCSV)
@@ -89,12 +102,18 @@ cargarDatosDesdeCSV(rutaArchivoCSV)
         // Llamar a la función para mostrar el árbol
         mostrarArbol(signals);
 
-        //filtrar las señales dadas usando expresiones regulares aplicadas a la columna MMSPATH.
+        // 3. Implementar una opción de búsqueda que permita filtrar las señales dadas usando
+        // expresiones regulares aplicadas a la columna MMSPATH.
         const expresionRegular = 'AXONBAYApplication';
         const senalesFiltradas = filtrarSenalesPorExpresionRegular(signals, expresionRegular);
 
         console.log(`Señales filtradas por la expresión regular "${expresionRegular}":`);
         console.table(senalesFiltradas);
+
+        // 4. Definir una función que retorne un número que mida la capacidad de una expresión de entrada
+        // para filtrar una señal y excluir el resto.
+        const capacidadExpresion = medirCapacidadExpresion(expresionRegular, signals);
+        console.log(`Capacidad de la expresión regular "${expresionRegular}": ${capacidadExpresion}`);
     })
     .catch((error) => {
         console.error('Error al cargar los datos:', error);
