033a9624ea38ff6c76bbf06febebf1e8d7f71c3b
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-12-21 00:01:14 -0500

    prueba terminada

--- app.js
+++ app.js
@@ -122,15 +122,63 @@ function proponerExpresionOptima(signals) {
     return expresionesOptimas;
 }
 
+
+
+
+// 6. Construir un algoritmo que genere una lista de expresiones candidatas por cada señal dada
+
+function generarExpresionesCandidatas(senal) {
+    const expresionesCandidatas = [];
+    const partes = senal.MMSPATH.split('/');
+
+    for (let i = 0; i < partes.length; i++) {
+        for (let j = i; j < partes.length; j++) {
+            const expresion = partes.slice(i, j + 1).join('/');
+            expresionesCandidatas.push(expresion);
+        }
+    }
+
+    return expresionesCandidatas;
+}
+
+
+
+
+// 7. Aplicar a cada expresión candidata la función definida en 4 y mostrar la expresión óptima
+
+function encontrarExpresionOptima(senal, expresionesCandidatas) {
+    let expresionOptima = expresionesCandidatas[0];
+    let capacidadOptima = medirCapacidadExpresion(expresionOptima, [senal]);
+
+    for (let i = 1; i < expresionesCandidatas.length; i++) {
+        const capacidadActual = medirCapacidadExpresion(expresionesCandidatas[i], [senal]);
+
+        if (capacidadActual < capacidadOptima) {
+            capacidadOptima = capacidadActual;
+            expresionOptima = expresionesCandidatas[i];
+        }
+    }
+
+    return expresionOptima;
+}
+
+
+
+//Ejecución de las funciones
+
 const rutaArchivoCSV = './archivo de entrada.csv';
 
 cargarDatosDesdeCSV(rutaArchivoCSV)
     .then((signals) => {
+        // 1. Cargar los datos del archivo CSV
         console.log('Datos cargados exitosamente:');
         console.table(signals);
+        console.log("----------------------------------------------------------------------------------------")
 
-        // Llamar a la función para mostrar el árbol
+        // 2. Llamar a la función para mostrar el árbol
         mostrarArbol(signals);
+        console.log("----------------------------------------------------------------------------------------")
+
 
         // 3. Implementar una opción de búsqueda que permita filtrar las señales dadas usando
         // expresiones regulares aplicadas a la columna MMSPATH.
@@ -139,17 +187,36 @@ cargarDatosDesdeCSV(rutaArchivoCSV)
 
         console.log(`Señales filtradas por la expresión regular "${expresionRegular}":`);
         console.table(senalesFiltradas);
+        console.log("----------------------------------------------------------------------------------------")
+
 
         // 4. Definir una función que retorne un número que mida la capacidad de una expresión de entrada
         // para filtrar una señal y excluir el resto.
         const capacidadExpresion = medirCapacidadExpresion(expresionRegular, signals);
         console.log(`Capacidad de la expresión regular "${expresionRegular}": ${capacidadExpresion}`);
+        console.log("----------------------------------------------------------------------------------------")
+
 
         // 5. Proponer para cada señal dada una expresión óptima para filtrarla.
         const expresionesOptimas = proponerExpresionOptima(signals);
 
         console.log('Expresiones óptimas para cada señal:');
         console.table(expresionesOptimas);
+        console.log("----------------------------------------------------------------------------------------")
+
+
+        // 6. Generar y mostrar expresiones candidatas para cada señal
+        signals.forEach((senal) => {
+            const expresionesCandidatas = generarExpresionesCandidatas(senal);
+            console.log(`Expresiones candidatas para ${senal.NOMBRE}:`);
+            console.log(expresionesCandidatas);
+            
+            
+        // 7. Aplicar a cada expresión candidata la función definida en el punto 4 y mostrar la expresión óptima
+            const expresionOptima = encontrarExpresionOptima(senal, expresionesCandidatas);
+            console.log(`Expresión óptima para ${senal.NOMBRE}: ${expresionOptima}`);
+            console.log("----------------------------------------------------------------------------------------")
+        });
     })
     .catch((error) => {
         console.error('Error al cargar los datos:', error);
